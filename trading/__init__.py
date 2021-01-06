439f3825120dfdb477f7085d4860b1ea4273157b
sawaca96 <sawaca96@hanmail.net>
2021-01-06 22:18:17 +0900

    refactor: set redis client

--- trading/__init__.py
+++ trading/__init__.py
@@ -4,11 +4,9 @@ from fastapi.responses import ORJSONResponse
 from gino import Gino
 
 from trading.config import DB_URL, CORS_ORIGINS, KAFKA_BOOTSTRAP_SERVERS
-from trading.redis import RedisClient
 from trading.producer import Producer
 
 db = Gino()
-redis_client = RedisClient()
 order_producer = Producer(
     "Order",
     bootstrap_servers=KAFKA_BOOTSTRAP_SERVERS,
@@ -66,13 +64,11 @@ def create_app():
     @app.on_event("startup")
     async def startup():
         await db.set_bind(DB_URL, min_size=1, echo=False)
-        await redis_client.get_conn(ignore_exception=True)
         await order_consumer.initialize()
 
     @app.on_event("shutdown")
     async def shutdown():
         await db.pop_bind().close()
-        await redis_client.close_pool()
         await order_consumer.stop()
 
     return app
--- trading/worker.py
+++ trading/worker.py
@@ -2,18 +2,24 @@ import asyncio
 
 import orjson
 import tenacity
+
+from trading.redis import RedisClient
 from trading.service import create_order
+from trading.broker import Broker
 
 
 class ConsumerWorker:
     def __init__(self, consumer):
         self.consumer = consumer
         self.consumer_task = None
+        self.redis_client = RedisClient()
 
     async def create_worker(self):
+        await self.redis_client.get_conn(ignore_exception=True)
         self.consumer_task = asyncio.create_task(self._consume_order())
 
     async def close_worker(self):
+        await self.redis_client.close_pool()
         await self.consumer_task.cancel()
 
     async def _consume_order(self):
@@ -33,10 +39,18 @@ class ConsumerWorker:
         finally:
             await self.consumer.stop()
 
-    @tenacity.retry(wait=tenacity.wait_fixed(2))
+    @tenacity.retry(wait=tenacity.wait_fixed(2), reraise=True)
     async def _commit_order(self, order):
         await create_order(order)
+        await self.redis_client.hset(order["id"], "order", orjson.dumps(order))
+        await self.register_order(order)
         await self.consumer.commit()
+
+    async def register_order(self, order):
+        name = "real:" + order["code"]
+        feed = await Broker.get(name)
+        if not feed.task or feed.task.done():
+            await feed.start_task()
 
     async def _consume_position(self):
         try:
