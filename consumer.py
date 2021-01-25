2c10db381b12846a9ccf358c149235505e221494
sawaca96 <sawaca96@hanmail.net>
2021-01-25 20:13:38 +0900

    feat: test-aioredlock

--- consumer.py
+++ consumer.py
@@ -1,17 +1,19 @@
-from aiokafka import AIOKafkaConsumer
 import asyncio
+from aiokafka import AIOKafkaConsumer
+
+import aioredis
+from aioredlock import Aioredlock
 
 
 async def consume():
     consumer = AIOKafkaConsumer(
-        "Order",
         bootstrap_servers=["localhost:9091", "localhost:9092", "localhost:9093"],
-        group_id="Test",
+        group_id="Order",
         enable_auto_commit=False,
     )
     # Get cluster layout and join group `my-group`
-    await consumer.start()
     try:
+        await consumer.start()
         # Consume messages
         async for msg in consumer:
             print(
@@ -29,4 +31,21 @@ async def consume():
         await consumer.stop()
 
 
-asyncio.run(consume())
+async def test_aioredlock():
+    redis = await aioredis.create_redis_pool(
+        address="redis://localhost:6379/1",
+        timeout=5,
+        encoding="utf-8",
+    )
+    redis_instance = [
+        "redis://localhost:6379/1",
+    ]
+    lock_manager = Aioredlock(redis_instance)
+
+    async with await lock_manager.lock("virtual-trading", lock_timeout=10):
+        keys = await redis.keys("*")
+        print(keys)
+
+
+# asyncio.run(consume())
+asyncio.run(test_aioredlock())
